6d327acf6b7284c6f3be1fe0f168621102f045ee
Valeria Kazakevich <71063002+Valeria154@users.noreply.github.com>
2025-03-12 11:57:20 +0300

    Delete dist directory

--- dist/index.91edd913.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0}}),$(document).ready(function(){$(".header_burger").click(function(e){$(".header_burger,.navigation").toggleClass("active"),$("body").toggleClass("lock")})});
-//# sourceMappingURL=index.91edd913.js.map
